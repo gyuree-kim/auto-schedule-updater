c1fb288b7e8b17cd4f1fe22ee35e14fcf63b68f6
gyuree.kim <gyuree.kim.dev@gmail.com>
2021-05-02 22:30:37 +0900

    1.7 Add api list

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,12 +11,140 @@ const ChatRooms = require('../models/chatRooms');
 const Message = require('../models/message');
 const Appointments = require('../models/appointments');
 
+// api list
+router.get("/", function (req, res, next) {
+  res.render("list", {
+    title: "api list",
+    apilist: [
+      // user
+      {
+        name: `${req.headers.host}/api/users`,
+        description: "get all users",
+        method: "get",
+      },
+      {
+        name: `${req.headers.host}/api/user/login`,
+        description: "user authentication",
+        method: "post",
+      },
+      {
+        name: `${req.headers.host}/api/user/register`,
+        description: "register new user",
+        method: "post",
+      },
+      {
+        name: `${req.headers.host}/api/user/search/:id`,
+        description: "search user by id",
+        method: "get",
+      },
+      {
+        name: `${req.headers.host}/api/user/search/:_id`,
+        description: "search user by _id",
+        method: "get",
+      },
+      {
+        name: `${req.headers.host}/api/user/search/:name`,
+        description: "search user by name",
+        method: "get",
+      },
+      {
+        name: `${req.headers.host}/api/user/update/:userId`,
+        description: "update user by userId",
+        method: "put",
+      },
+      // message
+      {
+        name: `${req.headers.host}/api/messages/:chatRoomId`,
+        description: "get all messages by chatRoomId",
+        method: "get",
+      },
+      {
+        name: `${req.headers.host}/api/message/:content`,
+        description: "get message with content",
+        method: "get",
+      },
+      {
+        name: `${req.headers.host}/api/message/create`,
+        description: "create new message",
+        method: "post",
+      },
+      {
+        name: `${req.headers.host}/api/message/remove/:messageId`,
+        description: "remove message by messageId",
+        method: "delete",
+      },
+      // appointment
+      {
+        name: `${req.headers.host}/api/appointment/create`,
+        description: "create a new appointment",
+        method: "post",
+      },
+      {
+        name: `${req.headers.host}/api/appointment/remove/:id`,
+        description: "remove appointment by id",
+        method: "delete",
+      },
+      {
+        name: `${req.headers.host}/api/appointment/update/:id`,
+        description: "update appointment by id",
+        method: "put",
+      },
+      {
+        name: `${req.headers.host}/api/appointments/:userId`,
+        description: "get all appointments by userId",
+        method: "get",
+      },
+      // chatroom
+      {
+        name: `${req.headers.host}/api/chatRoom/create/:userId`,
+        description: "create new chatRoom with userId",
+        method: "post",
+      },
+      {
+        name: `${req.headers.host}/api/chatRoom/search/:userId`,
+        description: "search chatRoom by userId",
+        method: "get",
+      },
+      {
+        name: `${req.headers.host}/api/chatRoom/remove/:chatRoomId`,
+        description: "remove chatRoom by chatRoomId",
+        method: "delete",
+      },
+      {
+        name: `${req.headers.host}/api/chatRoom/update/:chatRoomId`,
+        description: "update chatRoom by chatRoomId",
+        method: "put",
+      },
+      {
+        name: `${req.headers.host}/api/chatRooms/:userId`,
+        description: "get all chatRooms by userId",
+        method: "get",
+      },
+    ],
+  });
+});
+
+
 router.get('/', function(req, res) {
     res.send('Hello world!');
 });
 
+// get all users
+router.get('/user', (req, res) => {
+  const filter = {}; //find all users
+  User.find(filter, (err, userList)=>{
+    if(err) res.status(400).json({msg: `getAll users error`});
+    if(!userList) res.status(404).json({msg: `users not found`});
+    else
+    {
+      console.log('find userList 성공');
+      return res.status(200).json({userList});
+    }
+  })
+})
+
 // user register
-router.post('/register', (req, res)=>{
+router.post('user/register', (req, res)=>{
   const newUser = new User({
     name: req.body.name,
     id: req.body.id,
@@ -69,22 +197,8 @@ router.post('/register', (req, res)=>{
   });
 });
 
-// get all users
-router.get('/user/getAll', (req, res) => {
-  const filter = {}; //find all users
-  User.find(filter, (err, userList)=>{
-    if(err) res.status(400).json({msg: `getAll users error`});
-    if(!userList) res.status(404).json({msg: `users not found`});
-    else
-    {
-      console.log('find userList 성공');
-      return res.status(200).json({userList});
-    }
-  })
-})
-
 // search user by id
-router.get('/user/get/:id', (req, res) => {
+router.get('/user/search/:id', (req, res) => {
   User.findOne({id: req.params.id}, (err, user) => {
     if(err) res.status(400).json({error: `user findOne error`});
     if(!user) res.status(404).json({msg: `user not found`});
@@ -97,7 +211,7 @@ router.get('/user/get/:id', (req, res) => {
 })
 
 // search user by _id
-router.get('/user/get/:_id', (req, res) => {
+router.get('/user/search/:_id', (req, res) => {
   User.findOne({_id: req.params._id}, (err, user) => {
     if(err) res.status(400).json({error: `user findOne error`});
     if(!user) res.status(404).json({msg: `user not found`});
@@ -110,7 +224,7 @@ router.get('/user/get/:_id', (req, res) => {
 })
 
 // search user by name
-router.get('/user/get/:name', (req, res) => {
+router.get('/user/search/:name', (req, res) => {
   User.findOne({name: req.params.name}, (err, user) => {
     if(err) res.status(400).json({error: `user findOne error`});
     if(!user) res.status(404).json({msg: `user not found`});
@@ -167,7 +281,7 @@ router.post('/message/create', function(req, res){
 });
 
 // remove message by id
-router.delete('/message/delete/:messageId', function(req, res){
+router.delete('/message/remove/:messageId', function(req, res){
   message.findOneAndDelete({
     _id: req.params.messageId
   }, function(err){
