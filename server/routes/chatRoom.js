783a6c24d3679856d13125bbe4c7b245c951206c
gyuree.kim <gyuree.kim.dev@gmail.com>
2021-05-13 18:30:02 +0900

    [api] chatroom, message, appointment (#8)
    * 1.13 add chatroom post api by async way
    
    * 1.14 add message apis
    
    * [api] add apointment crud apis (#7)
    
    * [api] add chatroom get api
    
    * [api] update post message api

--- server/routes/chatRoom.js
+++ server/routes/chatRoom.js
@@ -4,37 +4,72 @@ var router = express.Router();
 const ChatRoom = require('../models/chatRooms');
 const User = require('../models/user');
 
-//create chatroom
+//create chatroom - ok
 router.post('/', (req, res) => {
-  const users = req.body.users; //user.id
-  const userIds = []; //user._id
-  for(var i = 0; i < users.length; i++) {
-    User.findOne({id: users[i]}, (err, user) => {
-      if(err) return res.status(500).send('db error')
-      else
-      {
-        userIds[i] = user._id
-      }
-    })
-  }
-  const chatRoom = new ChatRoom({
-    users: userIds,
-    createdAt: new Date
-  })
-  chatRoom.save((err) => {
-    if(err) res.status(500).send("db error")
-    else{
-      res.status(201).send(`success save ${chatRoom}`)
-    }
-  })
-})
+  try{
+    const userId = req.body.userId;
+    const invitedIds = req.body.invitedUsers;
 
-// get all chatRooms by user id
-router.get('/:userId', (req, res) => {
-  const filter = { id: req.params.userId };
+    getUser();
+    findUsers();
+    
+    async function getUser() 
+    {
+      const user = await User.findOne({ id: userId });
+      if(!user) {
+        throw new Error(`user not found`);
+        return res.status(404)
+      }
+      return user;
+    }
+
+    function delay(id)
+    {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          User.findOne({id:id}, (err, user)=>{
+            if(err) return res.status(500)
+            if(!user) return res.status(404).send("user not found");
+            else resolve(user);
+          })
+        }, 500);
+      });
+    }
+
+    async function findUsers() 
+    {
+      const promises = invitedIds.map(id => delay(id));
+      await Promise.all(promises);
+      console.log("found all users!");
+      createChatRoom();
+    }
+
+    function createChatRoom() 
+    {
+      invitedIds.push(userId[0])
+      const chatRoom = new ChatRoom({
+        users: invitedIds,
+        createdAt: new Date
+      })
+      chatRoom.save((err) => {
+        if(err) return res.status(500).send("error")
+        else
+        {
+          res.status(201).send(`success save ${chatRoom}`)
+        }
+      })
+    }
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+// get all chatRooms - ok
+router.get('/', (req, res) => {
+  const filter = {};
   ChatRoom.find(filter, (err, chatRooms)=>{
-    if(err) res.status(500).json({msg: `db error`});
-    if(!chatRooms) res.status(404).json({msg: `chatRoom not found`});
+    if(err) res.status(500).json({status:500, msg: `db error`});
+    if(chatRooms == '') res.status(404).json({status:404, msg: `chatRoom not found`});
     else
     {
       return res.status(200).send(`success find chatRooms ${chatRooms}`);
@@ -42,12 +77,39 @@ router.get('/:userId', (req, res) => {
   })
 })
 
-//get recent msg data
+// get all chatRooms by user id - ok
+router.get('/:userId', (req, res) => {
+  const filter = { 'users': { $in: [req.params.userId] } };
+  ChatRoom.find(filter, (err, chatRooms)=>{
+    if(err) res.status(500).json({status:500, msg: `db error`});
+    if(chatRooms == '') res.status(404).json({status:404, msg: `chatRoom not found`});
+    else
+    {
+      return res.status(200).send(`success find chatRooms ${chatRooms}`);
+    }
+  })
+})
+
+// get a chatRoom by _id - ok
+router.get('/_id/:_id', (req, res) => {
+  const filter = { _id: req.params._id };
+  ChatRoom.findOne(filter, (err, room)=>{
+    if(err) return res.status(500).send(err)
+    if(!room) return res.status(404).send(`chatRoom not found`);
+    else
+    {
+      return res.status(200).send(room)
+    }
+  })
+})
+
+//get recent msg data - ok
 router.get('/:chatRoomId/recent-message', (req, res) => {
-  const filter = { id: req.params.chatRoomId };
+  const filter = { _id: req.params.chatRoomId };
   ChatRoom.findOne(filter, (err, chatRoom)=>{
     if(err) res.status(500).json({msg: `db error`});
     if(!chatRoom) res.status(404).json({msg: `chatRoom not found`});
+    if(!chatRoom.recentMsg) res.status(404).send(`message not found`);
     else
     {
       return res.status(200).send(`success find chatRoom ${chatRoom.recentMsg}`);
@@ -56,25 +118,25 @@ router.get('/:chatRoomId/recent-message', (req, res) => {
 })
 
 // update chatRoom by _id
-router.put('/:_id', function(req, res){
-  ChatRoom.findOne({ _id: req.params._id }, function(err, chatRoom){
-    if(err) res.status(500).json({ error: 'database failure' } );
-    if(!chatRoom) res.status(404).json({ error: 'chatRoom not found' });
+router.put('/:chatRoomId', function(req, res){
+  const filter = { _id: req.params.chatRoomId };
+  ChatRoom.findOne(filter, function(err, chatRoom){
+    if(err) res.status(500).json({ msg: 'db failure' } );
+    if(!chatRoom) res.status(404).send( 'chatRoom not found' );
 
     if(req.body.recentMsg) chatRoom.recentMsg = req.body.recentMsg;
 
     chatRoom.save(function(err){
-      if(err) res.state(500).json({error: 'database error'});
+      if(err) res.status(500).send({error: 'database error'});
       else 
       {
-        res.json({message: 'chatRoom update 성공'});
-        res.status(200).json(chatRoom);
+        res.status(200).send(`status: 200, chatRoom: ${chatRoom}`);
       }
     });
   })
 })
 
-// remove chatRoom
+// remove chatRoom - ok
 router.delete('/_id/:_id', function(req, res){
   ChatRoom.findOne({_id: req.params._id}, (err, result) =>{
     result.remove( {}, function(err, result){  
--- server/routes/main/api_list.js
+++ server/routes/main/api_list.js
@@ -8,115 +8,125 @@ router.get("/", function (req, res, next) {
     apilist: [
       // user
       {
-        name: `${req.headers.host}/api/user`,
+        name: `${req.headers.host}/api/users`,
         description: "get all users",
         method: "get",
       },
       {
-        name: `${req.headers.host}/api/user/login`,
+        name: `${req.headers.host}/api/users/login`,
         description: "user authentication",
         method: "post",
       },
       {
-        name: `${req.headers.host}/api/user/register`,
+        name: `${req.headers.host}/api/users/register`,
         description: "register new user",
         method: "post",
       },
       {
-        name: `${req.headers.host}/api/user/id/:id`,
+        name: `${req.headers.host}/api/users/id/:id`,
         description: "search user by id",
         method: "get",
       },
       {
-        name: `${req.headers.host}/api/user/_id/:_id`,
+        name: `${req.headers.host}/api/users/_id/:_id`,
         description: "search user by _id",
         method: "get",
       },
       {
-        name: `${req.headers.host}/api/user/name/:name`,
+        name: `${req.headers.host}/api/users/name/:name`,
         description: "search users by name",
         method: "get",
       },
       {
-        name: `${req.headers.host}/api/user/:userId`,
+        name: `${req.headers.host}/api/users/:userId`,
         description: "update user by userId",
         method: "put",
       },
       {
-        name: `${req.headers.host}/api/user/:userId`,
+        name: `${req.headers.host}/api/users/:userId`,
         description: "update user by userId",
         method: "put",
       },
       {
-        name: `${req.headers.host}/api/user/:id`,
+        name: `${req.headers.host}/api/users/:id`,
         description: "get all messages by user id",
         method: "delete",
       },
       // message
       {
-        name: `${req.headers.host}/api/message/:content`,
+        name: `${req.headers.host}/api/messages/:content`,
         description: "get message with content",
         method: "get",
       },
       {
-        name: `${req.headers.host}/api/message/:_id`,
+        name: `${req.headers.host}/api/messages/:_id`,
         description: "get a message",
         method: "get",
       },
       {
-        name: `${req.headers.host}/api/message`,
+        name: `${req.headers.host}/api/messages`,
         description: "create new message",
         method: "post",
       },
       {
-        name: `${req.headers.host}/api/message/:messageId`,
+        name: `${req.headers.host}/api/messages/:messageId`,
         description: "remove message by messageId",
         method: "delete",
       },
       // appointment
       {
-        name: `${req.headers.host}/api/appointment`,
+        name: `${req.headers.host}/api/appointments`,
         description: "create a new appointment",
         method: "post",
       },
       {
-        name: `${req.headers.host}/api/appointment/:id`,
+        name: `${req.headers.host}/api/appointments/_id/:_id`,
         description: "remove appointment by id",
         method: "delete",
       },
       {
-        name: `${req.headers.host}/api/appointment/:id`,
+        name: `${req.headers.host}/api/appointments/_id/:_id`,
         description: "update appointment by id",
         method: "put",
       },
       {
-        name: `${req.headers.host}/api/appointments/:userId`,
+        name: `${req.headers.host}/api/appointments`,
+        description: "get all appointments",
+        method: "get",
+      },
+      {
+        name: `${req.headers.host}/api/appointments/userId/:userId`,
         description: "get all appointments by userId",
+        method: "get",
+      },
+      {
+        name: `${req.headers.host}/api/appointments/_id/:_id`,
+        description: "get all appointments by _id",
         method: "get",
       },
       // chatroom
       {
-        name: `${req.headers.host}/api/chatRoom/:userId`,
+        name: `${req.headers.host}/api/chatRooms/:userId`,
         description: "create new chatRoom with userId",
         method: "post",
       },
       {
-        name: `${req.headers.host}/api/chatRoom/:userId`,
+        name: `${req.headers.host}/api/chatRooms/:userId`,
         description: "get all chatRooms by userId",
         method: "get",
       },
       {
-        name: `${req.headers.host}/api/chatRoom/:chatRoomId/recent-message`,
+        name: `${req.headers.host}/api/chatRooms/:chatRoomId/recent-message`,
         description: "get recent message by chatRoomId",
         method: "get",
       },      
       {
-        name: `${req.headers.host}/api/chatRoom/:chatRoomId`,
+        name: `${req.headers.host}/api/chatRooms/:chatRoomId`,
         description: "update chatRoom by chatRoomId",
         method: "put",
       },
       {
-        name: `${req.headers.host}/api/chatRoom/:chatRoomId`,
+        name: `${req.headers.host}/api/chatRooms/:chatRoomId`,
         description: "remove chatRoom by chatRoomId",
         method: "delete",
       }
