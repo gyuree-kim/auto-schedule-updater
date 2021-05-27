6a7dc07dca12e061d381bd373b2838a2f87badb7
hexa6do@naver.com <hexa6do@hanyang.ac.kr>
2021-05-27 20:44:48 +0900

    Add infected list view

---
+++ frontend/app/src/main/java/com/android/frontend/InfectedActivity.java
@@ -0,0 +1,14 @@
+package com.android.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfectedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infected);
+    }
+}
