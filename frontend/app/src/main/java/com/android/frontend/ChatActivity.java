13b01f28b175a459a0f40ad7c0b0ddc134bc89da
hexa6do@naver.com <hexa6do@hanyang.ac.kr>
2021-05-27 13:07:08 +0900

    create assential chat view

--- frontend/app/src/main/java/com/android/frontend/ChatActivity.java
+++ frontend/app/src/main/java/com/android/frontend/ChatActivity.java
@@ -2,12 +2,24 @@ package com.android.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private Button btn_send;
+    private EditText et_msg;
+    private ListView lv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        lv = findViewById(R.id.lv_msglist);
+        btn_send = findViewById(R.id.btn_send_msg);
+        et_msg = findViewById(R.id.et_msg);
     }
 }
--- frontend/app/src/main/java/com/android/frontend/RoomsFragment.java
+++ frontend/app/src/main/java/com/android/frontend/RoomsFragment.java
@@ -1,5 +1,6 @@
 package com.android.frontend;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -33,47 +35,7 @@ public class RoomsFragment extends Fragment {
     private FloatingActionButton fab_add;
     private String userId;
     private ListView lv;
-
-//
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
-//
-//    public RoomsFragment() {
-//        // Required empty public constructor
-//    }
-//
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment RoomsFragment.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static RoomsFragment newInstance(String param1, String param2) {
-//        RoomsFragment fragment = new RoomsFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
+    private Button btn_gochat;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -87,12 +49,25 @@ public class RoomsFragment extends Fragment {
             userId = bundle.getString("userId");    //전달된 값
         }
 
-
         view = inflater.inflate(R.layout.fragment_rooms, container, false);
         lv = (ListView) view.findViewById(R.id.lv_chatroom_list);
         getRoomResponse();
         Log.d("rooms", "함수시작");
 
+        btn_gochat = (Button)view.findViewById(R.id.btn_gochat);
+        btn_gochat.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                //엑티비티를 dialog처럼 사용
+                Intent intent = new Intent(getActivity(), ChatActivity.class);
+                startActivity(intent); //getActivity().
+                //커스텀 다이어로그 클라스 넘기기
+//                AddroomDialog addroomDialog = new AddroomDialog(getActivity());
+//                addroomDialog.callFunction(userId);
+
+            }
+        });
 
         //-------추가 버튼 눌렀을때 검색창 dialog 띄우기
         FloatingActionButton fab = (FloatingActionButton)view.findViewById(R.id.fab_add);
