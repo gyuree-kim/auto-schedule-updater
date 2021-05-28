1834759db562d36678a50e573d00dd414c60816b
hexaspace <hexa6do@naver.com>
2021-05-29 00:10:40 +0900

    Ner138 infected view (#17)
    * Add infected list view
    
    * create infected view basic
    
    Co-authored-by: hexa6do@naver.com <hexa6do@hanyang.ac.kr>

--- frontend/app/src/main/java/com/android/frontend/ChatActivity.java
+++ frontend/app/src/main/java/com/android/frontend/ChatActivity.java
@@ -39,47 +39,47 @@ public class ChatActivity extends AppCompatActivity {
 //        chatAdapter=new ChatAdapter(msgItems, getLayoutInflater());
         //리스트뷰
         //        RoomList roomAdapter = new RoomList(this, last_message,last_notread, last_sender,last_time);//한번에 넣기
-        chatAdapter = new ChatAdapter();//한번에 넣을댄 여기 인자고 this, 배열, 배열
-        lv.setAdapter(chatAdapter);
-
-        //통신
-        RetrofitClient retrofitClient = new RetrofitClient();
-        Call<UserResponse> call = retrofitClient.server.getAllUsers();
-
-        call.enqueue(new Callback<UserResponse>() {
-            @Override
-            public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
-                Log.d("rooms", "응답 : " + String.valueOf(response.code()));
-
-                if(response.code() == 200){
-                    Log.d("rooms", "getusers 성공 : " + String.valueOf(response.code()));
-                    UserResponse ulist = response.body();
-                    //ArrayList<UserItem> user_list = response.body();
-                    for(UserItem item : ulist.items){
-                        String result = item.getId() + "id and "+ item.getName();
-                        userAdapter.addUserItem(item);
-
-                    }
-//                    if (user_list != null){
-//                        if (user_list.items != null && user_list.items.size()>0){
-//                            for (UserItem item : user_list.items){
-//                                userAdapter.addUserItem(item);
-//                            }
-//                        }
+//        chatAdapter = new ChatAdapter();//한번에 넣을댄 여기 인자고 this, 배열, 배열
+//        lv.setAdapter(chatAdapter);
+//
+//        //통신
+//        RetrofitClient retrofitClient = new RetrofitClient();
+//        Call<UserResponse> call = retrofitClient.server.getAllUsers();
+//
+//        call.enqueue(new Callback<UserResponse>() {
+//            @Override
+//            public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
+//                Log.d("rooms", "응답 : " + String.valueOf(response.code()));
+//
+//                if(response.code() == 200){
+//                    Log.d("rooms", "getusers 성공 : " + String.valueOf(response.code()));
+//                    UserResponse ulist = response.body();
+//                    //ArrayList<UserItem> user_list = response.body();
+//                    for(UserItem item : ulist.items){
+//                        String result = item.getId() + "id and "+ item.getName();
+//                        userAdapter.addUserItem(item);
+//
 //                    }
-
-                } else if (response.code() == 400) {
-//                    Toast.makeText(MainActivity.this, "user not found",
-//                            Toast.LENGTH_LONG).show();
-                    Log.d("rooms", "users not found" + String.valueOf(response.code()));
-                }
-            }
-            @Override
-            public void onFailure(Call<UserResponse> call, Throwable t) {
-                Log.d("rooms","response fail"+t.toString());
-                t.printStackTrace();
-            }
-        });
+////                    if (user_list != null){
+////                        if (user_list.items != null && user_list.items.size()>0){
+////                            for (UserItem item : user_list.items){
+////                                userAdapter.addUserItem(item);
+////                            }
+////                        }
+////                    }
+//
+//                } else if (response.code() == 400) {
+////                    Toast.makeText(MainActivity.this, "user not found",
+////                            Toast.LENGTH_LONG).show();
+//                    Log.d("rooms", "users not found" + String.valueOf(response.code()));
+//                }
+//            }
+//            @Override
+//            public void onFailure(Call<UserResponse> call, Throwable t) {
+//                Log.d("rooms","response fail"+t.toString());
+//                t.printStackTrace();
+//            }
+//        });
         //data
 //        String[] last_message={"마지막 메세지","최근 메세지","누군가 메세지","그냥 메세지"};
 //        int[] last_notread={1,2,3,1};
