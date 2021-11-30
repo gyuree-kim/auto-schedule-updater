2cf3257a5a5dfd0374620b3794d233f559543c05
gyuree.kim <gyuree.kim.dev@gmail.com>
2021-11-30 14:21:40 +0900

    NER-186: 파이썬 패키지와 api 연동 (#37)
    * NER-186: WIP
    
    * NER-186: 파이썬 프로세스 리턴 이슈 해결

--- server/routes/message.js
+++ server/routes/message.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 const User = require('../models/user');
 const Message = require('../models/message');
+const Event = require('../models/event');
+var pythonPackage = require('../python_pacakge')
+
+// data
+const message_content = '[서산시청] 5월 27일(목) 19시~21시까지 서산중앙병원 장례식장 별실 방문자는 6월 1일까지 보건소 선별진료소(09~18시까지)에서 검사 바랍니다.'		
 
 /// create message -ok
 router.post('/', function(req, res) {
@@ -28,6 +33,45 @@ router.post('/', function(req, res) {
       if(err) throw err;
       else return res.status(201).send(message);
     })
+
+    // 파이썬 패키지 결과 추출
+    async function get_package_result(context) {
+      var result = await pythonPackage.async_run_main(message_content);
+      return result
+    }
+    var result = get_package_result()
+    result.then( packageResult => {
+      console.log("PYTHON PACKAGE RESULT IS... " + packageResult)
+      _result =  packageResult.replace('[','').split('],')
+
+      var type, count, date, time
+      _result.forEach(value => {
+        _value = value.replace('[', '').replace(/'/g, '').split(', ')
+        _value[0] = _value[0].replace(' ', '')
+        if (_value[0] == 'type') { type = _value[1].toString() }
+        if (_value[0] == 'count') { count = _value[1].toString() }
+        if (_value[0] == 'date') { date = _value[1].toString() }
+        if (_value[0] == 'time') { time = _value[1].toString() }
+      });
+    })
+
+    // 이벤트 저장
+    const event = new Event({
+      messageId: message._id,
+      type: type,
+      count: count,
+      date: date,
+      time: time,
+      location: '',
+      createdAt: new Date,
+      updatedAt: new Date
+    })
+    console.log('event instance is created')
+    event.save((err) => {
+        if(err) throw err
+        else return res.status(201).send(event)
+    })
+
   } catch(e) {
     console.log(err)
     res.status(500).send(e);
